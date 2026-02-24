3dfb5e5d2a1a385648ce4b26818cbd1019d32f4b
Anton-Hladyshev <154468210+Anton-Hladyshev@users.noreply.github.com>
2026-02-24 14:13:23 +0100

    add Task class

---
+++ app/src/main/java/com/example/todolist/Task.kt
@@ -0,0 +1,36 @@
+package com.example.todolist
+
+import com.example.todolist.model.interfaces.Editable
+import java.util.Date
+import com.example.todolist.model.enums.State
+
+
+class Task(
+    val id: Int,
+    override var title: String,
+    override var description: String = "",
+    override var endDate: Date = Date(),
+    override var endTime: Date = Date(),
+    override var state: State = State.TODO,
+) : Editable {
+
+    override fun editTitle(newTitle: String) {
+        title = newTitle
+    }
+
+    override fun editDescription(newDescr: String) {
+        description = newDescr
+    }
+
+    override fun changeState(newState: State) {
+        state = newState
+    }
+
+    override fun changeEndTime(newEndTime: Date) {
+        endTime = newEndTime
+    }
+
+    override fun changeEndDate(newEndDate: Date) {
+        endDate = newEndDate
+    }
+}
