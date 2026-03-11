9dc3dda54bd2bba7b89f921a1087d7a4b1cf7d8f
Anton-Hladyshev <154468210+Anton-Hladyshev@users.noreply.github.com>
2026-03-11 10:42:02 +0100

    update the filter panel: add priority and periodicity filters

--- app/src/main/java/com/example/todolist/model/Filter.kt
+++ app/src/main/java/com/example/todolist/model/Filter.kt
@@ -1,18 +1,21 @@
 package com.example.todolist.model
 import com.example.todolist.model.enums.State
 import com.example.todolist.model.enums.Periodicity
+import com.example.todolist.model.enums.Priority
 import com.example.todolist.model.interfaces.TaskSpecification
 import java.util.Date
 
 data class Filter(
     var stateFilter: State? = null,
     var periodicityFilter: Periodicity? = null,
+    var priorityFilter: Priority? = null,
     var endDateFilter: Date? = null,
     var endTimeFilter: Date? = null
 ) : TaskSpecification {
     override fun isSatisfiedBy(task: Task) : Boolean {
         return (stateFilter == null || task.state == stateFilter) &&
                 (periodicityFilter == null || task.periodicity == periodicityFilter) &&
+                (priorityFilter == null || task.priority == priorityFilter) &&
                 (endDateFilter == null || !task.deadline.time.after(endDateFilter)) &&
                 (endTimeFilter == null || !task.deadline.time.after(endTimeFilter))
     }
